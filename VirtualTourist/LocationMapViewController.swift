9222dd150411b42dc5737a46b86e189fd4d9504a
Jeffersonbe <jefferson.bonnaire@gmail.com>
2016-02-07 23:11:52 +0000

    Create UI

---
+++ VirtualTourist/LocationMapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LocationMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Jefferson Bonnaire on 07/02/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+// PhotoAlbumViewController.swift
 //  VirtualTourist
 //
 //  Created by Jefferson Bonnaire on 07/02/2016.
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var toolBarButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
