87f2a0fbfb3c484705b679d017d79000b78297a9
Jeffersonbe <jefferson.bonnaire@gmail.com>
2016-02-07 22:14:40 +0000

    Create Models

---
+++ VirtualTourist/Photo.swift
@@ -0,0 +1,38 @@
+//
+//  Photo.swift
+//  VirtualTourist
+//
+//  Created by Jefferson Bonnaire on 07/02/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import CoreData
+
+class Photo : NSManagedObject {
+
+    struct Keys {
+        static let Title = "title"
+        static let imageUrl = "url_m"
+    }
+
+    @NSManaged var title: String
+    @NSManaged var imageUrl: String
+
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+
+    init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
+
+        // Core Data
+        let entity =  NSEntityDescription.entityForName("Photo", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+
+        // Dictionary
+        title = dictionary[Keys.Title] as! String
+        imageUrl = dictionary[Keys.imageUrl] as! String
+    }
+}
---
+++ VirtualTourist/Pin.swift
@@ -0,0 +1,38 @@
+//
+//  Pin.swift
+//  VirtualTourist
+//
+//  Created by Jefferson Bonnaire on 07/02/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import CoreData
+
+class Pin : NSManagedObject {
+
+    struct Keys {
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+    }
+
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+
+    init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
+
+        // Core Data
+        let entity =  NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+
+        // Dictionary
+        latitude = dictionary[Keys.Latitude] as! Double
+        longitude = dictionary[Keys.Longitude] as! Double
+    }
+}
